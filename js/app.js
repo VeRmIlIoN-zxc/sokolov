339f65381ee4b07e05945be5c365634130fa6459
VeRmIlIoN-zxc <ghostfacelight271@gmail.com>
2025-11-06 19:52:46 +0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,17 +1,2 @@
-function sum(a,b) 
-{
-    return a.value+b.value    
-}
 
-
-
-let btn = document.querySelector('section button')
-
-btn.addEventListener('click', function (){
-    let nums = document.querySelector('section input')
-    let rezult = document.querySelector('section p')
-    rezult.innerHTML = `Сумма: ${nums[0].value} + ${nums[1].value} = ${sum(nums[0], nums[1])}`
-})
-
-
-
+    
